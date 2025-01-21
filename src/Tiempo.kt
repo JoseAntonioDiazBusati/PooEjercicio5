d8e067ce0126008d3d54dd84d215edde53d5e993
JoseAntonio <jdiabus511@g.educaand.es>
2025-01-21 17:30:52 +0100

    ajuste ejercicio5

--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -5,7 +5,7 @@ class Tiempo (var hora: Int, var minuto: Int, var segundo: Int) {
     //constructor(hora: Int, minuto: Int,segundo: Int): this(hora,0,0)
 
     init {
-        require(hora in 0..23) {"No pueden ser más de 24 horas o menos."}
+        require(hora in 0..23) {"No pueden ser más de 24 horas o menos de 0."}
         require(minuto >= 0) {"No pueden ser menos de 0 minutos."}
         require(segundo >= 0) {"No pueden ser menos de 0 segundos."}
     }
